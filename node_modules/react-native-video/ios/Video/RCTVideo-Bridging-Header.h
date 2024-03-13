a1441390f5ca940f37a7f8afe3d8a666797af2b6
munkyuhwan <55966981+munkyuhwan@users.noreply.github.com>
2024-03-13 14:13:37 +0900

    gitignore

--- node_modules/react-native-video/ios/Video/RCTVideo-Bridging-Header.h
+++
@@ -1,8 +0,0 @@
-#import <React/RCTViewManager.h>
-#import "RCTVideoSwiftLog.h"
-#import "RCTEventDispatcher.h"
-
-#if __has_include(<react-native-video/RCTVideoCache.h>)
-#import "RCTVideoCache.h"
-#endif
-
