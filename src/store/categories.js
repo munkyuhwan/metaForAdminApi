6e17b3939bfc3b24039d5d7e66436f45553ecf05
munkyuhwan <55966981+munkyuhwan@users.noreply.github.com>
2024-04-26 16:10:28 +0900

    중분류 카테고리 노출 여부 수정

--- src/store/categories.js
+++ src/store/categories.js
@@ -40,7 +40,12 @@ export const setSubCategories = createAsyncThunk("categories/setSubCategories", 
     const subCategoreis = allCategories.filter(item => item.cate_code1 == selectedMainCategory);
     if(subCategoreis.length>0) {
         const subLevel = subCategoreis[0]?.level2;
-        const filteredSubLevel = subLevel.filter(el=>(el.is_use == "Y" && el.is_del=="N" ));
+        var filteredSubLevel = [];
+        if(subLevel?.length>0) {
+            filteredSubLevel = subLevel.filter(el=>(el.is_use == "Y" && el.is_del=="N" ));
+        }else {
+            filteredSubLevel = subLevel;
+        }
         return filteredSubLevel;
     }else {
         return[]
