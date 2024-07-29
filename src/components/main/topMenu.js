57d6075fa12ad33e98e19cdf5be6ccb42d0f7cbf
munkyuhwan <55966981+munkyuhwan@users.noreply.github.com>
2024-07-29 11:21:03 +0900

    2.2.12
    - cctv화면에서 메인화면 안넘어가도록 수정
    - cctv테이블별 사용여부 적용.
    - cctv화면에서 메인화면 안넘어가도록 수정

--- src/components/main/topMenu.js
+++ src/components/main/topMenu.js
@@ -36,7 +36,7 @@ const TopMenu = () =>{
     const {selectedMainCategory,subCategories, allCategories} = useSelector(state => state.categories);
     const [tableNoText, setTableNoText] = useState("");
     const [tableInfoText, setTableInfoText] = useState("");
-    const {tableInfo,cctv} = useSelector(state => state.tableInfo);
+    const {tableInfo,cctv,tableStatus} = useSelector(state => state.tableInfo);
     
     const {bulletin} = useSelector(state=>state.menuExtra);
 
@@ -164,7 +164,7 @@ const TopMenu = () =>{
                         </AutoScroll>
                     }
                 </SafeAreaView>
-                {cctv?.length > 0 &&
+                { (tableStatus?.is_cctv=="Y" && cctv?.length > 0)&&
                     <TouchableWithoutFeedback onPress={()=>{openTransperentPopup(dispatch, {innerView:"CameraView", isPopupVisible:true}); }} >
                         <Image style={{width:50,height:50, position:'absolute',right:'40%'}} resizeMode='contain' source={require("../../assets/icons/cctv.png")}/>
                     </TouchableWithoutFeedback>
--- src/resources/releaseNote.js
+++ src/resources/releaseNote.js
@@ -1,6 +1,12 @@
 
-export const CURRENT_VERSION = "2.2.11";
+export const CURRENT_VERSION = "2.2.12";
 export const releaseNote = {
+    "2.2.12": //2024.07.27
+    `
+        - cctv화면에서 메인화면 안넘어가도록 수정
+        - cctv테이블별 사용여부 적용.
+        - cctv화면에서 메인화면 안넘어가도록 수정
+    `,
     "2.2.11": //2024.07.26
     `
         - cctv화면에서 메인화면 안넘어가도록 수정
